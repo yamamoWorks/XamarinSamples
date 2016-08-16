8b45d29771791e9c4922bb2e6e185504c2f108f4
yamamo <mail.to.yamamo@gmail.com>
2016-08-16 17:52:32 +0900

    new

---
+++ ActivityIndicatorRenderer/ActivityIndicatorRenderer/ActivityIndicatorRenderer.UWP/CustomActivityIndicatorRenderer.cs
@@ -0,0 +1,87 @@
+﻿using ActivityIndicatorRenderer.UWP;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.UWP;
+
+[assembly: ExportRenderer(typeof(ActivityIndicator), typeof(CustomActivityIndicatorRenderer))]
+
+namespace ActivityIndicatorRenderer.UWP
+{
+    public class CustomActivityIndicatorRenderer : ViewRenderer<ActivityIndicator, Windows.UI.Xaml.Controls.ProgressRing>
+    {
+        private Brush foregroundDefault;
+
+        protected override void OnElementChanged(ElementChangedEventArgs<ActivityIndicator> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.NewElement != null)
+            {
+                if (Control == null)
+                {
+                    SetNativeControl(new Windows.UI.Xaml.Controls.ProgressRing());
+                    Control.Loaded += OnControlLoaded;
+                }
+                UpdateIsRunning();
+            }
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+
+            if (e.PropertyName == ActivityIndicator.IsRunningProperty.PropertyName)
+            {
+                UpdateIsRunning();
+            }
+            else if (e.PropertyName == ActivityIndicator.ColorProperty.PropertyName)
+            {
+                UpdateColor();
+            }
+        }
+
+        void OnControlLoaded(object sender, RoutedEventArgs routedEventArgs)
+        {
+            foregroundDefault = Control.Foreground;
+            UpdateColor();
+        }
+
+        void UpdateColor()
+        {
+            Color color = Element.Color;
+            if (color == Color.Default)
+            {
+                Control.Foreground = foregroundDefault;
+            }
+            else
+            {
+                Control.Foreground = color.ToBrush();
+            }
+        }
+
+        void UpdateIsRunning()
+        {
+            Control.IsActive = Element.IsRunning;
+        }
+    }
+
+    internal static class ConvertExtensions
+    {
+        public static Brush ToBrush(this Color color)
+        {
+            return new SolidColorBrush(color.ToWindowsColor());
+        }
+
+        public static Windows.UI.Color ToWindowsColor(this Color color)
+        {
+            return Windows.UI.Color.FromArgb((byte)(color.A * 255), (byte)(color.R * 255), (byte)(color.G * 255), (byte)(color.B * 255));
+        }
+    }
+}
---
+++ ActivityIndicatorRenderer/ActivityIndicatorRenderer/ActivityIndicatorRenderer/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace ActivityIndicatorRenderer
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            pk1.SelectedIndexChanged += Pk1_SelectedIndexChanged;
+        }
+
+        private void Pk1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.ai1.Color = (Color)new ColorTypeConverter().ConvertFrom(pk1.Items[pk1.SelectedIndex]);
+        }
+    }
+}
